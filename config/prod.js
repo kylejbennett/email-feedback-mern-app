b0a76b13c9f87da2bc2e795be9562140b574502e
Kyle Bennett <kyle.j.bennett5@gmail.com>
2020-07-30 14:23:25 -0400

    finished mongo setup, adding production db and keys

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    GOOGLE_OAUTH_CLIENT_ID: process.env.GOOGLE_OAUTH_CLIENT_ID,
+    GOOGLE_OAUTH_CLIENT_SECRET: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+    MONGO_URI: process.env.MONGO_URI,
+    COOKIE_KEY: process.env.COOKIE_KEY
+};
--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require("express");
-require('./services/passport');
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require('./models/User');
+require("./services/passport");
+
+mongoose.connect(keys.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to DB!'))
 
 const app = express();
 
-require('./routes/authRoutes')(app);
+app.use(
+    cookieSession({
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      keys: [keys.COOKIE_KEY]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
