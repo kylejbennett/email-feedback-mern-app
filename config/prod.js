c9cfc53dbbee9ffcc678ddb6ecf6fcfdf8c316cb
Kyle Bennett <kyle.j.bennett5@gmail.com>
2020-08-07 11:49:22 -0400

    added billing and client side app

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,7 @@ module.exports = {
     GOOGLE_OAUTH_CLIENT_ID: process.env.GOOGLE_OAUTH_CLIENT_ID,
     GOOGLE_OAUTH_CLIENT_SECRET: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
     MONGO_URI: process.env.MONGO_URI,
-    COOKIE_KEY: process.env.COOKIE_KEY
+    COOKIE_KEY: process.env.COOKIE_KEY,
+    STRIPE_PUB_KEY: process.env.STRIPE_PUB_KEY,
+    STRIPE_SECRET: process.env.STRIPE_SECRET
 };
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 require('./models/User');
 require("./services/passport");
@@ -17,6 +18,8 @@ db.once('open', () => console.log('Connected to DB!'))
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use(
     cookieSession({
       maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -28,6 +31,17 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    // if route not recognized, it will return the index file
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
